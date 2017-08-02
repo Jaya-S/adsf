2541659b2212518feceb277095ff7aec1baec64d
Jaya-S <jaivedgiri@gmail.com>
2017-08-02 11:50:06 +0530

    Create stringtpoint.java

---
+++ stringtpoint.java
@@ -0,0 +1,16 @@
+# adsf
+import java.io.*;
+
+public class stringtpoint {
+public static void main(String args[]) throws  IOException{
+	
+	String s="200";
+	int i=Integer.parseInt(s);
+	System.out.println(s+100);
+	System.out.println(i+100);
+	
+			
+	
+	
+}
+}
